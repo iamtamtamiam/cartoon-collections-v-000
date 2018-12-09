f923b4d96fa6722692c896c850f8095631ab14e2
Tam Vu <vutam_a@hotmail.com>
2018-12-09 17:16:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  i = 0
+  dwarves.each_with_index {|name, i| puts ""
 end
 
 def summon_captain_planet# code an argument here
